79288e063ba3ee5b9fa42d6bf67214b6f7a87f43
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-09-29 19:25:20 -0400

    install vite

---
+++ src/js/buttons/enable-grid.js
@@ -0,0 +1,32 @@
+class Attempt {
+	constructor(eventQuery) {
+		const currentEvent = this;
+		eventQuery.forEach((query) => {
+			query.addEventListener('click', this.toggleGrid);
+		});
+	}
+	toggleGrid() {
+		const grid = document.querySelector('#word_grid');
+		const gridBtns = grid.querySelectorAll('.word-btn');
+
+		this.state = grid.getAttribute('data-grid-state');
+
+		grid.setAttribute(
+			'data-grid-state',
+			this.state === 'disabled' ? 'enabled' : 'disabled',
+		);
+
+		this.state = grid.getAttribute('data-grid-state');
+		this.state !== 'disabled' && grid.focus();
+
+		gridBtns.forEach((btn) => {
+			btn.attributes.item(1)
+				? btn.removeAttribute('disabled')
+				: btn.setAttribute('disabled', '');
+		});
+
+		console.log(this.state);
+	}
+}
+
+export { Attempt };
