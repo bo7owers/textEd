15bfe75e780bd9141dabad70e09cd796cfbb71fc
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-10-06 17:46:26 -0400

    add more content

--- src/js/content/populate-content.js
+++ src/js/content/populate-content.js
@@ -6,15 +6,23 @@ class initGame {
 	async populateInstructions(instructionList) {
 		try {
 			let resp = await fetch('./questions.json');
-			let instructions = await resp.json();
+			let instructions = await resp.json();	
 			console.log(instructions.questions[0]);
 
 			// iterate over the questions and create an li element for each description
 			for (const question of instructions.questions) {
 				let listItem = document.createElement('li');
+				let paragraph = document.createElement('p');
 				let wordDiv = document.createElement('div');
+				let editBtn = document.createElement('button');
+
 				wordDiv.className = 'word-container';
-				wordDiv.textContent = question.description;
+				paragraph.textContent = question.description;
+
+				editBtn.classList.add('btn', 'btn-edit');
+
+				wordDiv.append(paragraph);
+				wordDiv.append(editBtn);
 				listItem.append(wordDiv);
 				instructionList.append(listItem);
 			}
